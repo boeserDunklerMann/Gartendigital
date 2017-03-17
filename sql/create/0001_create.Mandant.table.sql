0ccfc3427175fe1c21dfe1e6bc3a8b07d9d1aac7
boeserDunklerMann <me@andre-dunkel.com>
2017-03-17 16:52:37 +0100

    Kleinigkeiten vergessen

--- sql/create/0001_create.Mandant.table.sql
+++ sql/create/0001_create.Mandant.table.sql
@@ -4,6 +4,7 @@ create TABLE Mandant
     MandantID   int not null auto_increment,
     ParentMandantID int null,
     Des VARCHAR(50) character set 'utf8' null,
+    SecurityToken BINARY(40) null,
     constraint PK_Mandant PRIMARY KEY(MandantID),
     CONSTRAINT FK_Mandant_Mandant foreign key (ParentMandantID) REFERENCES Mandant(MandantID)
 );
