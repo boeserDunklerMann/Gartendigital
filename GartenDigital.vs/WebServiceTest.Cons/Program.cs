34c3694f88b7c75a3ea7c24b80d55226f2aad798
boeserDunklerMann <me@andre-dunkel.com>
2017-03-18 17:32:59 +0100

    Webservice.Cons TestApp added
    Testanwendung für GDCommHub.Mereen-Webservice

---
+++ GartenDigital.vs/WebServiceTest.Cons/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServiceTest.Cons
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ServiceReference1.addPortTypeClient test = new ServiceReference1.addPortTypeClient();
+			int x = test.add(3, 5);
+			
+		}
+	}
+}
