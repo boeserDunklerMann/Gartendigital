72e2eb5967118a1580edd550f4e54ca966fe909c
boeserDunklerMann <me@andre-dunkel.com>
2017-03-22 21:18:11 +0100

    CalcPipeline with DB doesn't work

--- IoT/ExecPipeline.php
+++ IoT/ExecPipeline.php
@@ -9,7 +9,7 @@
 //    if (msg_receive($mq, 0, $_msgTypeNewRaven, 40, $ravenID, true, MSG_IPC_NOWAIT) == true)
     if (msg_receive($mq, 0, $_msgTypeNewRaven, 40, $ravenID, true, MSG_IPC_NOWAIT) != true)
     {
-        $ravenID = 54;
+        $ravenID = 8;
     }
     {
         // msg_remove_queue($mq);
@@ -22,6 +22,7 @@
         $res->data_seek(0);
         $row = $res->fetch_assoc();
         $ravenMsg = $row["Message"];
+        $res->free();
 
         $xml = simplexml_load_string($ravenMsg);
         $sensors = $xml->Data->Sensors;
@@ -34,7 +35,47 @@
             $meteringName = $sensor["MeteringName"];
             $vmp = $sensor["VirtualMeteringPoint"];
             $meteringValue = $sensor;
-            printf("Name: %s - Type: %s - Value: %s - VMP: %d\n", $meteringName, $meteringType, $meteringValue, $vmp);
+            // printf("Name: %s - Type: %s - Value: %s - VMP: %d\n", $meteringName, $meteringType, $meteringValue, $vmp);
+            $sql = sprintf("select CalculationPipelineID from VirtualMeteringPoint where VmpID=%d and MandantID=%d", $vmp, $mandant);
+            syslog(LOG_DEBUG, $sql);
+            $result = $mysql->query($sql);
+
+            if ($result)
+            {
+                if ($result->num_rows === 0)
+                    syslog(LOG_DEBUG, "no calc pipeline found.");
+                else
+                    syslog(LOG_DEBUG, "Entering calc pipeline");
+
+                while($result->num_rows !== 0)
+                {
+                    $row = $result->fetch_assoc();
+                    $calcpipeid = $row["CalculationPipelineID"];
+                    $result->free();
+                    syslog(LOG_DEBUG, sprintf("fetching calc pipeline #%d", $calcpipeid));
+                    $result = $mysql->query(sprintf("select * from CalculationPipeline where CalculationPipelineID=%d", $calcpipeid));
+                    if ($result)
+                    {
+                        $row = $result->fetch_assoc();
+                        // TODO: berechne (dafür brauch ich die $row)
+                        // hole nächsten
+                        $result->free();
+                        $sql = sprintf("select * from CalculationPipeline where ParentCalcID=%d", $calcpipeid);
+                        $result = $mysql->query($sql);
+                        // if ($result)
+                        // {
+                        //     $row = $result->fetch_assoc();
+                        // }
+                    }
+                }
+            }
+            else
+            {
+                echo "Error: Our query failed to execute and here is why: \n";
+                echo "Query: " . $sql . "\n";
+                echo "Errno: " . $mysql->errno . "\n";
+                echo "Error: " . $mysql->error . "\n";
+            }
         }
     }
     syslog(LOG_DEBUG, "exiting execution pipeline");
