412d0bfa60787ee6cf90c830a5e50fe1ea8ec0dd
boeserDunklerMann <me@andre-dunkel.com>
2017-03-29 15:35:27 +0200

    netstats

--- Shell/updateNetRrd.sh
+++ Shell/updateNetRrd.sh
@@ -2,7 +2,6 @@
 # Meereen
 # update networktraffic-rrd-db, minutly, called by cron
 # 29.03.2017
-rx=` ifconfig eth0 | awk 'FNR==8 {print $3}' | tr '(' ' '`
-tx=` ifconfig eth0 | awk 'FNR==8 {print $7}' | tr '(' ' '`
-tot=`echo "$rx+$tx" | bc`
+tot=`echo "scale=1;($(cat /sys/class/net/eth0/statistics/tx_bytes)+$(cat /sys/class/net/eth0/statistics/rx_bytes))/1024" | bc -l`
+echo $tot
 rrdtool update /var/Meereen.Comm.Hub/rrd/networkrxtx.rrd N:$tot
